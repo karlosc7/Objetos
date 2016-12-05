b29671e207d292c341ea54f34dee8185b801bc19
karlosc7 <carlosc7fuen@gmail.com>
2016-12-05 20:20:10 +0100

    Initial commit. Primeros pasos con objetos en javascript. Movimientos de un héroe

---
+++ ejemplo.js
@@ -0,0 +1,35 @@
+/**
+ * Created by cdelc on 29/11/2016.
+ */
+function moverIzquierda(){
+    this.moverIzq=function(){
+        document.write(nom+" se mueve a la izquierda.");
+    }
+    this.moverIzq=function(){
+        document.write(nom+" se mueve a la izquierda.");
+    }
+}
+
+function Heroe(nom, arm){
+
+    this.nombre=nom;
+    this.arma=arm;
+
+    this.moverIzquierda=moverIzquierda();
+
+    this.moverDer=function(){
+        document.write(nom+" se mueve a la derecha.");
+    }
+    this.moverDer=function(){
+        document.write(nom+" se mueve a la derecha.");
+    }
+function Datos(){
+    this.mostrarDatos=function(){
+        document.write("Nombre del héroe: "+nombre+ " <br>con el arma"+arma);
+    }
+
+}
+    this.mostrarDatos=function(){
+        document.write("Nombre del héroe: "+nombre+ " <br>con el arma"+arma);
+    }
+}
